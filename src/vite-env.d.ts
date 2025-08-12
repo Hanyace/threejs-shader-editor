67fcaca43f3386c6753ab97f4b964989139f30b7
Mike Liu <mike.liu@auralabs.cn>
2025-08-12 09:10:30 +0800

    deploy: update env variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@
 // vite env types
 interface ImportMetaEnv {
   readonly VITE_GITHUB_LINK: string;
+  readonly VITE_TITLE: string;
+  readonly VITE_BASE_PATH: string;
 }
 
 interface ImportMeta {
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { fileURLToPath } from "url";
 
 // https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd());
+    return {
+        base: env.VITE_BASE_PATH ?? "/",
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                "@": fileURLToPath(new URL("./src", import.meta.url)),
+            },
         },
-    },
+    };
 });
